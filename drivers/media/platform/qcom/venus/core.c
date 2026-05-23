4fac6845ce2f121e9c075d27892c98fde6baffa0
Gourav Kumar <gouravk@qti.qualcomm.com>
2026-05-23 09:25:58 +0530

    Revert "FROMLIST: media: qcom: venus: flip the venus/iris switch"
    This reverts commit 46f670f2b85181106503ed4d1c597e080c4c3c3e.
    
    Signed-off-by: Gourav Kumar <gouravk@qti.qualcomm.com>
    (cherry picked from commit 572f9d98870c168b59a75961e5a60efece21e0b8)

--- drivers/media/platform/qcom/venus/core.c
+++ drivers/media/platform/qcom/venus/core.c
@@ -968,7 +968,6 @@ static const struct venus_resources sc7180_res = {
 	.enc_nodename = "video-encoder",
 };
 
-#if (!IS_ENABLED(CONFIG_VIDEO_QCOM_IRIS))
 static const struct freq_tbl sm8250_freq_table[] = {
 	{ 0, 444000000 },
 	{ 0, 366000000 },
@@ -1170,7 +1169,6 @@ static const struct venus_resources sc8280xp_res = {
 	.cp_nonpixel_size = 0x24800000,
 	.fwname = "qcom/vpu-2.0/venus.mbn",
 };
-#endif
 
 static const struct bw_tbl qcm2290_bw_table_dec[] = {
 	{ 352800, 597000, 0, 746000, 0 }, /* 1080p@30 + 720p@30 */
@@ -1227,15 +1225,11 @@ static const struct of_device_id venus_dt_match[] = {
 	{ .compatible = "qcom,msm8998-venus", .data = &msm8998_res, },
 	{ .compatible = "qcom,qcm2290-venus", .data = &qcm2290_res, },
 	{ .compatible = "qcom,sc7180-venus", .data = &sc7180_res, },
-#if (!IS_ENABLED(CONFIG_VIDEO_QCOM_IRIS))
 	{ .compatible = "qcom,sc7280-venus", .data = &sc7280_res, },
-#endif
 	{ .compatible = "qcom,sdm660-venus", .data = &sdm660_res, },
 	{ .compatible = "qcom,sdm845-venus", .data = &sdm845_res, },
 	{ .compatible = "qcom,sdm845-venus-v2", .data = &sdm845_res_v2, },
-#if (!IS_ENABLED(CONFIG_VIDEO_QCOM_IRIS))
 	{ .compatible = "qcom,sm8250-venus", .data = &sm8250_res, },
-#endif
 	{ }
 };
 MODULE_DEVICE_TABLE(of, venus_dt_match);
